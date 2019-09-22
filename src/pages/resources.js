20523935607542a0756a44b86143307e82ba02f2
CodeForFaith <codeforfaith@gmail.com>
2019-09-22 20:07:29 +0200

    add resources page

---
+++ src/pages/resources.js
@@ -0,0 +1,178 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ResourcesPage = () => {
+
+  return (
+  <Layout>
+    <SEO title="QR" />
+    <article className="resource">
+        <div className="title">Bible:</div>
+        <ul>
+          <li>
+            Bible.is:
+            <a href="http://www.bible.is/" target="_blank">Website</a>
+          </li>
+          <li>
+            King James Bible Online:
+            <a href="https://www.kingjamesbibleonline.org/" target="_blank"
+              >Website</a
+            >
+          </li>
+        </ul>
+      </article>
+      <article className="resource">
+        <div className="title">Truthers:</div>
+        <ul>
+          <li>
+            Nicholson1968:
+            <a href="https://www.nicholson1968.com/" target="_blank">Website</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/user/nicholson1968"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            Shaking My Head Productions:
+            <a
+              href="https://www.youtube.com/channel/UCk_uPqSuvOP8zaankyDjz7Q"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            the Truth is stranger than fiction...:
+            <a
+              href="https://weseeasthroughaglassdarkly.wordpress.com/"
+              target="_blank"
+              >Blog</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/channel/UC2aFQcGgRoVuEWfx2AM0H-A"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            Face Like The Sun:
+            <a
+              href="https://www.youtube.com/user/FaceLikeTheSun"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            The Watchman:
+            <a href="https://www.youtube.com/user/777tjm/" target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            ThePottersClay:
+            <a
+              href="https://drive.google.com/file/d/0BywqxaR-5McuYl9ua25GUmlORjA/view"
+              target="_blank"
+              >Book</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/channel/UCpEdOLvL6olNghR-BWghUQA"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            Shofar Ministries:
+            <a
+              href="https://www.youtube.com/channel/UCjkZx8ouJVtxuIEFSbItFHA"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+        </ul>
+      </article>
+      <article className="resource">
+        <div className="title">Ministries:</div>
+        <ul>
+          <li>
+            Faith Comes By Hearing:
+            <a href="https://www.faithcomesbyhearing.com/" target="_blank"
+              >Website</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/user/faithcomesbyhearing/"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            Chick Publications:
+            <a href="https://www.chick.com/" target="_blank">Website</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/user/pdflm"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            Torch of Christ Ministries:
+            <a href="http://torchofchrist.com/" target="_blank">Website</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/channel/UCKYcfq8_yAieQRZ81M_A7hw"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            Motivating U2Win:
+            <a href="https://www.motivatingu2win.com" target="_blank">Website</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/channel/UCdF6Wk5yJJVnK5dAOnGyzeA"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+        </ul>
+      </article>
+      <article className="resource">
+        <div className="title">Apologetics:</div>
+        <ul>
+          <li>
+            Proof That God Exists:
+            <a href="http://www.proofthatgodexists.org/" target="_blank"
+              >Website</a
+            >
+          </li>
+        </ul>
+      </article>
+      <article className="resource">
+        <div className="title">Music:</div>
+        <ul>
+          <li>
+            Destiny Lab:
+            <a href="https://www.destinylab.com/" target="_blank">Website</a
+            >&nbsp;|&nbsp;<a
+              href="https://www.youtube.com/user/destinylab"
+              target="_blank"
+              >YouTube</a
+            >
+          </li>
+          <li>
+            Godly Christian Music:
+            <a href="http://www.godlychristianmusic.com/" target="_blank"
+              >Website</a
+            >
+          </li>
+        </ul>
+      </article>
+      <article className="resource">
+        <div className="title">API:</div>
+        <ul>
+          <li>
+            Coming soon...
+          </li>
+        </ul>
+      </article>
+  </Layout>
+)};
+
+export default ResourcesPage
