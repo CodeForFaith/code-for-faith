a7d288746f3450f7158a98b13eea17a478b22ca2
CodeForFaith <codeforfaith@gmail.com>
2019-09-22 16:46:11 +0200

    add header compoent

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,45 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
+// import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <>
+    <header className="header">
+      <div className="cross">
+        _|_
+        <br />
+        &nbsp;|
+        <br />
+      </div>
+      <div className="code">Code</div>
+      <div className="for">for</div>
+      <div className="faith">faith</div>
+    </header>
+    <nav className="menu">
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/updates">updates</Link>
+        </li>
+        <li>
+          <Link to="/resources">resources</Link>
+        </li>
+        <li>
+          <Link to="/contact">contact</Link>
+        </li>
+      </ul>
+    </nav>
+  </>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  // siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  // siteTitle: ``,
 }
 
 export default Header
