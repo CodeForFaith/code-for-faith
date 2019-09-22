b51b09039e0f761eb87f1f01b23698cc83e68138
CodeForFaith <codeforfaith@gmail.com>
2019-09-22 19:46:20 +0200

    add contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const ContactPage = () => {
+
+  return (
+  <Layout>
+    <SEO title="Home" />
+    <article className="contact">
+        <p>Email: <a href="mailto:codeforfaith@gmail.com">codeforfaith@gmail.com</a></p>
+      </article>
+  </Layout>
+)};
+
+export default ContactPage
