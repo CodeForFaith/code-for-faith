891e62ac41d2c991529cf1b8f555b49087a76640
RilDev <c.public@tutanota.com>
2020-04-04 15:58:17 +0200

    improvement(src/pages): add spaces after columns

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -4,90 +4,106 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const ResourcesPage = () => {
-
   return (
-  <Layout>
-    <SEO title="Resources" />
-    <article className="resource">
-        <div className="title">Bible:</div>
+    <Layout>
+      <SEO title="Resources" />
+      <article className="resource">
+        <div className="title">Bible:&nbsp;</div>
         <ul>
           <li>
-            Bible.is:
-            <a href="http://www.bible.is/" target="_blank">Website</a>
+            Bible.is:&nbsp;
+            <a href="http://www.bible.is/" target="_blank">
+              Website
+            </a>
           </li>
           <li>
-            King James Bible Online:
-            <a href="https://www.kingjamesbibleonline.org/" target="_blank"
-              >Website</a
-            >
+            King James Bible Online:&nbsp;
+            <a href="https://www.kingjamesbibleonline.org/" target="_blank">
+              Website
+            </a>
           </li>
         </ul>
       </article>
       <article className="resource">
-        <div className="title">Truthers:</div>
+        <div className="title">Truthers:&nbsp;</div>
         <ul>
           <li>
-            Nicholson1968:
-            <a href="https://www.nicholson1968.com/" target="_blank">Website</a
-            >&nbsp;|&nbsp;<a
+            Nicholson1968:&nbsp;
+            <a href="https://www.nicholson1968.com/" target="_blank">
+              Website
+            </a>
+            &nbsp;|&nbsp;
+            <a
               href="https://www.youtube.com/user/nicholson1968"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
           <li>
-            Shaking My Head Productions:
+            Shaking My Head Productions:&nbsp;
             <a
               href="https://www.youtube.com/channel/UCk_uPqSuvOP8zaankyDjz7Q"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
           <li>
-            the Truth is stranger than fiction...:
+            the Truth is stranger than fiction...:&nbsp;
             <a
               href="https://weseeasthroughaglassdarkly.wordpress.com/"
               target="_blank"
-              >Blog</a
-            >&nbsp;|&nbsp;<a
+            >
+              Blog
+            </a>
+            &nbsp;|&nbsp;
+            <a
               href="https://www.youtube.com/channel/UC2aFQcGgRoVuEWfx2AM0H-A"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
           <li>
-            Face Like The Sun:
+            Face Like The Sun:&nbsp;
             <a
               href="https://www.youtube.com/user/FaceLikeTheSun"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
           <li>
-            The Watchman:
-            <a href="https://www.youtube.com/user/777tjm/" target="_blank"
-              >YouTube</a
-            >
+            The Watchman:&nbsp;
+            <a href="https://www.youtube.com/user/777tjm/" target="_blank">
+              YouTube
+            </a>
           </li>
           <li>
-            ThePottersClay:
+            ThePottersClay:&nbsp;
             <a
               href="https://drive.google.com/file/d/0BywqxaR-5McuYl9ua25GUmlORjA/view"
               target="_blank"
-              >Book</a
-            >&nbsp;|&nbsp;<a
+            >
+              Book
+            </a>
+            &nbsp;|&nbsp;
+            <a
               href="https://www.youtube.com/channel/UCpEdOLvL6olNghR-BWghUQA"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
           <li>
-            Shofar Ministries:
+            Shofar Ministries:&nbsp;
             <a
               href="https://www.youtube.com/channel/UCjkZx8ouJVtxuIEFSbItFHA"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
         </ul>
       </article>
@@ -95,7 +111,7 @@ const ResourcesPage = () => {
         <div className="title">Ministries:</div>
         <ul>
           <li>
-            Revelations of Jesus Christ
+            Revelations of Jesus Christ:&nbsp;
             <a href="https://theghettogospel.com/home/" target="_blank">
               Website
             </a>
@@ -108,41 +124,53 @@ const ResourcesPage = () => {
             </a>
           </li>
           <li>
-            Faith Comes By Hearing:
-            <a href="https://www.faithcomesbyhearing.com/" target="_blank"
-              >Website</a
-            >&nbsp;|&nbsp;<a
+            Faith Comes By Hearing:&nbsp;
+            <a href="https://www.faithcomesbyhearing.com/" target="_blank">
+              Website
+            </a>
+            &nbsp;|&nbsp;
+            <a
               href="https://www.youtube.com/user/faithcomesbyhearing/"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
           <li>
-            Chick Publications:
-            <a href="https://www.chick.com/" target="_blank">Website</a
-            >&nbsp;|&nbsp;<a
-              href="https://www.youtube.com/user/pdflm"
-              target="_blank"
-              >YouTube</a
-            >
+            Chick Publications:&nbsp;
+            <a href="https://www.chick.com/" target="_blank">
+              Website
+            </a>
+            &nbsp;|&nbsp;
+            <a href="https://www.youtube.com/user/pdflm" target="_blank">
+              YouTube
+            </a>
           </li>
           <li>
-            Torch of Christ Ministries:
-            <a href="http://torchofchrist.com/" target="_blank">Website</a
-            >&nbsp;|&nbsp;<a
+            Torch of Christ Ministries:&nbsp;
+            <a href="http://torchofchrist.com/" target="_blank">
+              Website
+            </a>
+            &nbsp;|&nbsp;
+            <a
               href="https://www.youtube.com/channel/UCKYcfq8_yAieQRZ81M_A7hw"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
           <li>
-            Motivating U2Win:
-            <a href="https://www.motivatingu2win.com" target="_blank">Website</a
-            >&nbsp;|&nbsp;<a
+            Motivating U2Win:&nbsp;
+            <a href="https://www.motivatingu2win.com" target="_blank">
+              Website
+            </a>
+            &nbsp;|&nbsp;
+            <a
               href="https://www.youtube.com/channel/UCdF6Wk5yJJVnK5dAOnGyzeA"
               target="_blank"
-              >YouTube</a
             >
+              YouTube
+            </a>
           </li>
         </ul>
       </article>
@@ -150,10 +178,10 @@ const ResourcesPage = () => {
         <div className="title">Apologetics:</div>
         <ul>
           <li>
-            Proof That God Exists:
-            <a href="http://www.proofthatgodexists.org/" target="_blank"
-              >Website</a
-            >
+            Proof That God Exists:&nbsp;
+            <a href="http://www.proofthatgodexists.org/" target="_blank">
+              Website
+            </a>
           </li>
         </ul>
       </article>
@@ -161,31 +189,31 @@ const ResourcesPage = () => {
         <div className="title">Music:</div>
         <ul>
           <li>
-            Destiny Lab:
-            <a href="https://www.destinylab.com/" target="_blank">Website</a
-            >&nbsp;|&nbsp;<a
-              href="https://www.youtube.com/user/destinylab"
-              target="_blank"
-              >YouTube</a
-            >
+            Destiny Lab:&nbsp;
+            <a href="https://www.destinylab.com/" target="_blank">
+              Website
+            </a>
+            &nbsp;|&nbsp;
+            <a href="https://www.youtube.com/user/destinylab" target="_blank">
+              YouTube
+            </a>
           </li>
           <li>
-            Godly Christian Music:
-            <a href="http://www.godlychristianmusic.com/" target="_blank"
-              >Website</a
-            >
+            Godly Christian Music:&nbsp;
+            <a href="http://www.godlychristianmusic.com/" target="_blank">
+              Website
+            </a>
           </li>
         </ul>
       </article>
       <article className="resource">
         <div className="title">API:</div>
         <ul>
-          <li>
-            Coming soon...
-          </li>
+          <li>Coming soon...</li>
         </ul>
       </article>
-  </Layout>
-)};
+    </Layout>
+  )
+}
 
 export default ResourcesPage
