84caa7ede112ea3ce668fb823f492ef2fc727ba5
RilDev <c.public@tutanota.com>
2020-04-05 17:47:58 +0200

    improvement(src/pages): privacy-policy change codeforfaith@gmail.com to contact@codeforfaith.com

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -16,7 +16,7 @@ const PrivacyPolicyPage = () => {
         <h2>4th of April 2020 (version 0.1.1)</h2>
         <p>This privacy notice tells you about the information we collect from you when you use our website. In collecting this information, we are acting as a data controller and, by law, we are required to provide you with information about us, about why and how we use your data, and about the rights you have over your data.</p>
         <h2>Who are we?</h2>
-        <p>We are Code for Faith. You can contact us by email at <a href="mailto:codeforfaith@gmail.com">codeforfaith@gmail.com</a>.</p>
+        <p>We are Code for Faith. You can contact us by email at <a href="mailto:contact@codeforfaith.com">contact@codeforfaith.com</a>.</p>
         <h2>How we use your information</h2>
         <ul>
             <li><a href="#when-you-use-our-website">When you use our website</a></li>
@@ -27,7 +27,7 @@ const PrivacyPolicyPage = () => {
 
         <h2 name="when-you-use-our-website">When you use our website</h2>
         <p>When you use our website we do not collect any data from you without your consent. We are not tracking you or using any third party service such as Google Analytics. And we are not going to share or sell your data.</p>
-        <p>However we may collect personal information from you such as your name or email address. For example, you may voluntarily contact us by email at <a href="mailto:codeforfaith@gmail.com">codeforfaith@gmail.com</a>.</p>
+        <p>However we may collect personal information from you such as your name or email address. For example, you may voluntarily contact us by email at <a href="mailto:contact@codeforfaith.com">contact@codeforfaith.com</a>.</p>
         <p>Through this website you are able to link to other websites which are not under the control of codeforfaith.com. We have no control over the nature, content and availability of those sites. The inclusion of any links does not necessarily imply a recommendation or endorse the views expressed within them.</p>
         <p>We do not use any cookies to run this website.</p>
         {/* <p>The only cookies that we use are strictly necessary. For more information about our use of cookies, please see our <a href="/cookie-policy">cookie policy</a>.</p> */}
@@ -39,7 +39,7 @@ const PrivacyPolicyPage = () => {
         <p>If we are processing your personal data for reasons of consent or legitimate interest, you can request that your data be erased.</p>
         <p>You have the right to ask us to stop using your information for a period of time if you believe we are not doing so lawfully.</p>
         <p>Finally, in some circumstances you can ask us not to reach decisions affecting you using automated processing or profiling.</p>
-        <p>To submit a request regarding your personal data please contact us at this email: <a href="mailto:codeforfaith@gmail.com">codeforfaith@gmail.com</a>.</p>
+        <p>To submit a request regarding your personal data please contact us at this email: <a href="mailto:contact@codeforfaith.com">contact@codeforfaith.com</a>.</p>
         
         <h2 name="liability">Liability</h2>
         <p>You are free to use our website or not. When reading our content and using our services you do so at your own risk.</p>
