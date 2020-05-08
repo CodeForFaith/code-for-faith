7003a19c17e8bf57b919f1eda3199b4f03bc5b16
RilDev <c.public@tutanota.com>
2020-05-09 01:20:44 +0200

    improvement(src/pages): add markdown graphql query

--- src/pages/updates.js
+++ src/pages/updates.js
@@ -1,12 +1,41 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const UpdatesPage = () => {
+export const query = graphql`
+  query UpdatesPageQuery {
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "MM/DD/YYYY")
+            title
+          }
+          html
+          id
+        }
+      }
+    }
+  }
+`
+
+const UpdatesPage = ({ data }) => {
   return (
     <Layout>
       <SEO title="Updates" />
+      {data.allMarkdownRemark.edges.map(edge => (
+        <article key={edge.node.id} className="update">
+          <p>
+            date: {edge.node.frontmatter.date}
+            <br />
+            title: {edge.node.frontmatter.title}
+          </p>
+          <div dangerouslySetInnerHTML={{ __html: edge.node.html }} />
+          <hr />
+        </article>
+      ))}
       <article className="update">
         <p>
           date: 05/26/2020
