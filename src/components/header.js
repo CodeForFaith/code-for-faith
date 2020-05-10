90f774480f718b082699066944cea531aa39c9dd
RilDev <c.public@tutanota.com>
2020-05-10 10:59:30 +0200

    improvement(/home/rildev/Projects/code-for-faith/website/src/components): add toggling langage choice

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,62 @@
 import { Link } from "gatsby"
 // import PropTypes from "prop-types"
-import React from "react"
+import React, { useContext } from "react"
+import Context from "../context/context"
 
-const Header = ({ siteTitle }) => (
-  <>
-    <header className="header">
-      <div className="cross">
-        _|_
-        <br />
-        &nbsp;|
-        <br />
+const Header = ({ siteTitle }) => {
+  const { state, setState } = useContext(Context)
+
+  const onClickLanguage = language => {
+    setState({ ...state, language })
+  }
+
+  return (
+    <>
+      <div className={`languages`}>
+        <span
+          className={`nav-link ${state.language === "en" ? "underline" : ""}`}
+          onClick={() => onClickLanguage("en")}
+        >
+          en
+        </span>
+        &nbsp;|&nbsp;
+        <span
+          className={`nav-link ${state.language === "fr" ? "underline" : ""}`}
+          onClick={() => onClickLanguage("fr")}
+        >
+          fr
+        </span>
       </div>
-      <div className="code">Code</div>
-      <div className="for">for</div>
-      <div className="faith">Faith</div>
-    </header>
-    <nav className="menu">
-      <ul>
-        <li>
-          <Link to="/">home</Link>
-        </li>
-        <li>
-          <Link to="/updates">updates</Link>
-        </li>
-        <li>
-          <Link to="/resources">resources</Link>
-        </li>
-        <li>
-          <Link to="/contact">contact</Link>
-        </li>
-      </ul>
-    </nav>
-  </>
-)
+      <header className="header">
+        <div className="cross">
+          _|_
+          <br />
+          &nbsp;|
+          <br />
+        </div>
+        <div className="code">Code</div>
+        <div className="for">for</div>
+        <div className="faith">Faith</div>
+      </header>
+      <nav className="menu">
+        <ul>
+          <li>
+            <Link to="/">home</Link>
+          </li>
+          <li>
+            <Link to="/updates">updates</Link>
+          </li>
+          <li>
+            <Link to="/resources">resources</Link>
+          </li>
+          <li>
+            <Link to="/contact">contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  )
+}
 
 Header.propTypes = {
   // siteTitle: PropTypes.string,
